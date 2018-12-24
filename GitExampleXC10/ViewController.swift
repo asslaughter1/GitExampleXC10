95ba7a82ec9c510e17010b65ab6ed1c0d94919fa
Alistair Slaughter <asslaughter1@cps.edu>
2018-12-23 22:20:52 -0600

    alistair sup edits

--- GitExampleXC10/ViewController.swift
+++ GitExampleXC10/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: NSViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        print("some change")
+        print("some changes like sup bruh Alistair branch yeeep")
     }
 
     override var representedObject: Any? {
