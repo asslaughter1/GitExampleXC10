d3a2cd9559a9f8b298fbeb88a56dc2d1fb2ca27a
Alistair Slaughter <asslaughter1@cps.edu>
2018-12-23 22:09:49 -0600

    Initial Commit

---
+++ GitExampleXC10/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GitExampleXC10
+//
+//  Created by Slaughter, Alistair S on 12/23/18.
+//  Copyright © 2018 Slaughter, Alistair S. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GitExampleXC10/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GitExampleXC10
+//
+//  Created by Slaughter, Alistair S on 12/23/18.
+//  Copyright © 2018 Slaughter, Alistair S. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
